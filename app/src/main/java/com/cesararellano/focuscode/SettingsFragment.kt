c1fc49e6e491cec62bf642d6a2fbd87b1df0c049
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-10-10 18:53:50 -0500

    Adding Settings Fragment UI

--- app/src/main/java/com/cesararellano/focuscode/SettingsFragment.kt
+++ app/src/main/java/com/cesararellano/focuscode/SettingsFragment.kt
@@ -5,14 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 
 class SettingsFragment : Fragment() {
 
+    override fun onResume() {
+        super.onResume()
+        buildDropdownItems(requireView())
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        buildDropdownItems(view)
+        return view
+    }
+
+    private fun buildDropdownItems(view: View) {
+        val themes = resources.getStringArray(R.array.themes)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.theme_dropdown_item, themes)
+        val themeDropdown = view.findViewById<AutoCompleteTextView>(R.id.themeDropdown)
+        themeDropdown.setAdapter( arrayAdapter )
     }
 }
