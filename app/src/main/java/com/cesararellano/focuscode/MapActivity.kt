0c97316f46762ac7d9d9586823b1e3bbfb2ecf52
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-10-13 14:47:26 -0500

    Adding Google Maps with scan place location

--- app/src/main/java/com/cesararellano/focuscode/MapActivity.kt
+++ app/src/main/java/com/cesararellano/focuscode/MapActivity.kt
@@ -2,12 +2,49 @@ package com.cesararellano.focuscode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var map: GoogleMap
+    private lateinit var placeLocation: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-        val placeLocation = intent.getStringExtra("PLACE_LOCATION")
-        println("placeLocation: $placeLocation")
+        placeLocation = intent.getStringExtra("PLACE_LOCATION") ?: "geo:-25.363,131.044"
+        println(placeLocation)
+        createFragment( )
+
+    }
+
+    private fun createFragment() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createMarker( getLatLng(placeLocation) )
+    }
+
+    private fun createMarker(coordinates: LatLng) {
+        val marker = MarkerOptions().position(coordinates).title("Lugar escaneado")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 18f), // Zoom in floats
+            1000,
+            null
+        )
+    }
+
+    private fun getLatLng(scanCode:String): LatLng {
+        val latLng = scanCode.substring(4).split(',')
+        val lat = latLng[0].toDouble()
+        val lng = latLng[1].toDouble()
+        return LatLng(lat, lng)
     }
 }
--- app/src/main/java/com/cesararellano/focuscode/ScanItem.kt
+++ app/src/main/java/com/cesararellano/focuscode/ScanItem.kt
@@ -2,7 +2,6 @@ package com.cesararellano.focuscode
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.android.gms.maps.model.LatLng
 import java.io.Serializable
 
 @Entity(tableName = "scans")
@@ -12,12 +11,4 @@ class ScanItem(
     var scanType: String,
     @PrimaryKey(autoGenerate = true)
     var scanId: Int = 0,
-    ): Serializable {
-
-    private fun getLatLng():LatLng {
-        val latLng = this.scanCode.substring(4).split(',');
-        val lat = latLng[0].toDouble();
-        val lng = latLng[1].toDouble();
-        return LatLng(lat, lng);
-    }
-}
+    ): Serializable
