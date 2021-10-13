be1052c9cbd495e39b8cfccf6c9fbbe10a58dbc9
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-10-13 14:06:30 -0500

    Adding share codes feature

--- app/src/main/java/com/cesararellano/focuscode/GeneratorFragment.kt
+++ app/src/main/java/com/cesararellano/focuscode/GeneratorFragment.kt
@@ -1,7 +1,11 @@
 package com.cesararellano.focuscode
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,10 +15,15 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.FileProvider
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.common.BitMatrix
 import com.journeyapps.barcodescanner.BarcodeEncoder
+import java.io.File
+import java.io.FileOutputStream
 import java.lang.Exception
 
 class GeneratorFragment : Fragment() {
@@ -46,7 +55,7 @@ class GeneratorFragment : Fragment() {
         }
 
         shareButton.setOnClickListener {
-            println("Shared Button click")
+            shareScreenshot()
         }
 
 
@@ -78,6 +87,54 @@ class GeneratorFragment : Fragment() {
         val barcodeEncoder = BarcodeEncoder()
         val bitmap: Bitmap = barcodeEncoder.createBitmap(bitMatrix)
         barcodeImage.setImageBitmap(bitmap)
+    }
+
+    private fun takeScreenshot(): Uri {
+
+        val containerView = requireView().findViewById<ConstraintLayout>(R.id.codeContainer)
+        val bitmap = Bitmap.createBitmap(containerView.width, containerView.height, Bitmap.Config.ARGB_8888)
+        //Bind a canvas to it
+        val canvas = Canvas(bitmap)
+        //Get the view's background
+        val bgDrawable = containerView.background
+        if (bgDrawable != null) //has background drawable, then draw it on the canvas
+            bgDrawable.draw(canvas)
+        else  //does not have background drawable, then draw white background on the canvas
+            canvas.drawColor(Color.WHITE)
+        // draw the view on the canvas
+        containerView.draw(canvas)
+        val screenshotFile = File(requireContext().externalCacheDir, "codes.jpeg")
+        val fOutStream = FileOutputStream(screenshotFile)
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOutStream)
+        fOutStream.flush()
+        fOutStream.close()
+
+        return FileProvider.getUriForFile(
+            requireContext(),
+            BuildConfig.APPLICATION_ID + ".provider",  //(app signature + ".provider" )
+            screenshotFile
+        )
+    }
+    private fun shareScreenshot() {
+        try {
+            val imageUri = takeScreenshot()
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                flags =  Intent.FLAG_ACTIVITY_NEW_TASK
+                putExtra(Intent.EXTRA_STREAM, imageUri)
+                type = "image/jpeg"
+            }
+            startActivity( Intent.createChooser(shareIntent, "Compartiendo código") )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Toast.makeText(
+                requireContext(),
+                "Ocurrió un error al compartir códigos",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
     }
 
     private fun View.hideKeyboard() {
