ce5142d23a435f71c742079d388b070d256dad49
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-10-13 22:25:50 -0500

    Adding night mode

--- app/src/main/java/com/cesararellano/focuscode/SettingsFragment.kt
+++ app/src/main/java/com/cesararellano/focuscode/SettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.cesararellano.focuscode
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import androidx.appcompat.app.AppCompatDelegate
 
 class SettingsFragment : Fragment() {
 
@@ -30,5 +33,16 @@ class SettingsFragment : Fragment() {
         val arrayAdapter = ArrayAdapter(requireContext(), R.layout.theme_dropdown_item, themes)
         val themeDropdown = view.findViewById<AutoCompleteTextView>(R.id.themeDropdown)
         themeDropdown.setAdapter( arrayAdapter )
+
+        themeDropdown.setOnItemClickListener{ _, _, position, _ ->
+            val theme = when(position) {
+                0 -> AppCompatDelegate.MODE_NIGHT_NO
+                1 -> AppCompatDelegate.MODE_NIGHT_YES
+                else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+            }
+
+            AppCompatDelegate.setDefaultNightMode(theme)
+        }
+
     }
 }
